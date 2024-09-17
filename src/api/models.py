fa0ffa054cd51ae8258db8b2b3c0b710b11a2cf9
Michflorez <167869106+Michflorez@users.noreply.github.com>
2024-09-17 12:30:04 +0000

    [añado end points]

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # or your database
+app.config['JWT_SECRET_KEY'] = 'your_jwt_secret'
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+CORS(app)
 
 db = SQLAlchemy()
 
--- src/api/routes.py
+++ src/api/routes.py
@@ -7,9 +7,53 @@ from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+@api.route('/signup', methods=['POST'])
+def create_user():
+    request_body = request.json
+    user_query = User.query.filter_by(email=request_body['email']).first()
+    if user_query is None:
+        create_user = User(email=request_body['email'],  password=request_body['password'], is_active=request_body['is_active'])
+        db.session.add(create_user)
+        db.session.commit()
+        response_body = {
+            "msg": "User created successfully"
+        }
+        return jsonify(response_body), 200
+    else:
+        response_body = {
+            "msg": "User already exists"
+        }
+        return jsonify(response_body), 404
+    
+@api.route('/login', methods=['POST'])
+def login_user():
+    request_body = request.json
+    email = request_body.get("email")
+    password = request_body.get("password")
+    user_login = User.query.filter_by(email = request_body["email"]).first()
+    if user_login is None:
+        response_body = {
+             "msg": "Usuario no existe"
+            }
+        return jsonify(response_body), 404
+    elif email != user_login.email or password != user_login.password:
+        return jsonify({"msg": "Usuario o contraseña incorrecta"}), 404
+    else:
+        access_token = create_access_token(identity= user_login.id)
+        return jsonify({ "token": access_token })
+    
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
 
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    return jsonify({"id": user.id}), 200
+            
